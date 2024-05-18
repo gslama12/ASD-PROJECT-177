d5d795b7d72221fe4df30e9d61e51abf1bc08bab
Simon Mussbacher <simon.mussbacher@aon.at>
2024-05-18 15:21:45 +0200

    Added missing special chars

--- quizcraft/client/src/components/UIComponents/Login/LoginComponent.jsx
+++ quizcraft/client/src/components/UIComponents/Login/LoginComponent.jsx
@@ -21,7 +21,7 @@ function LoginComponent() {
         const hasUpperCase = /[A-Z]/.test(password);
         const hasLowerCase = /[a-z]/.test(password);
         const hasNumber = /[0-9]/.test(password);
-        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|\-<>_´=§/+]/.test(password);
         return (
             password.length >= minLength &&
             hasUpperCase &&
