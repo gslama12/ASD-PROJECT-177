7eea69bd44471835c51ce7a860c214b169c494a2
Georg Slamanig <georgslamanig@MacBook-Pro-von-Georg.local>
2024-06-18 11:13:06 +0200

    fixed mobile screen for home component

--- quizcraft/client/src/components/UIComponents/Home/HomeComponent.jsx
+++ quizcraft/client/src/components/UIComponents/Home/HomeComponent.jsx
@@ -8,24 +8,26 @@ function HomeComponent({ socket }) {
     return (
         <>
             <div className="home-container">
-                <div className="mode-cards-container">
-                    <div className="mode-card quiz-card">
-                        <Link to="/quizmode" className="mode-link">
-                            <h2>Quiz</h2>
-                            <p>Try your knowledge in various topics!</p>
-                        </Link>
-                    </div>
-                    <div className="mode-card trivia-card">
-                        <Link to="/triviamode" className="mode-link">
-                            <h2>Trivia</h2>
-                            <p>Go full on trivia mode and reach highscores!</p>
-                        </Link>
-                    </div>
-                    <div className="mode-card challenges-card">
-                        <Link to="/challengemode" className="mode-link">
-                            <h2>Challenges</h2>
-                            <p>Test your skills with our weekly challenges!</p>
-                        </Link>
+                <div className={"home-content"}>
+                    <div className="mode-cards-container">
+                        <div className="mode-card quiz-card">
+                            <Link to="/quizmode" className="mode-link">
+                                <h2>Quiz</h2>
+                                <p>Try your knowledge in various topics!</p>
+                            </Link>
+                        </div>
+                        <div className="mode-card trivia-card">
+                            <Link to="/triviamode" className="mode-link">
+                                <h2>Trivia</h2>
+                                <p>Go full on trivia mode and reach highscores!</p>
+                            </Link>
+                        </div>
+                        <div className="mode-card challenges-card">
+                            <Link to="/challengemode" className="mode-link">
+                                <h2>Challenges</h2>
+                                <p>Test your skills with our weekly challenges!</p>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
